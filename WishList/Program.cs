64c95626fde12af8e288b78746e07ee844d973bd
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:25:29 +0200

    first commit

---
+++ WishList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\data\wishlist.txt";
+            List<string> dataFromFile = File.ReadAllLines(filePath).ToList();
+            Console.WriteLine("Enter your first wish: ");
+            string firstwish = Console.ReadLine();
+            Console.WriteLine("Enter your second wish: ");
+            string secondwish = Console.ReadLine();
+            Console.WriteLine("Enter your final wish: ");
+            string thirdwish = Console.ReadLine();
+
+            dataFromFile.Add(firstwish);
+            File.WriteAllLines(filePath, dataFromFile);
+            dataFromFile.Add(secondwish);
+            File.WriteAllLines(filePath, dataFromFile);
+            dataFromFile.Add(thirdwish);
+            File.WriteAllLines(filePath, dataFromFile);
+        }
+    }
+}
---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\data\test.txt";
+            List<string> dataFromFile = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine($"Data from file: {line}");
+            }
+
+
+            dataFromFile.Add("grapes");
+            File.WriteAllLines(filePath, dataFromFile);
+        }
+    }
+}
